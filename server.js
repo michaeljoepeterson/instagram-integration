5825f2e788e72a1d0a6d87b973f9ee1a53849faa
Michael Peterson <michaeljoepeterson@gmail.com>
2020-06-14 17:01:50 -0600

    initial connection to database

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 const express = require('express');
-const {PORT } = require('./config');
+const {PORT, DATABASE_URL,DOMAINS } = require('./config');
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
 const app = express();
 app.use(jsonParser);
-app.set('trust proxy', true)
+//app.set('trust proxy', true)
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization,Accept');
@@ -16,9 +16,23 @@ app.use(function (req, res, next) {
     }
     next();
 });
-
+/* prod
+app.use(function (req, res, next) {
+  let origin = req.headers.origin;
+  let allowedOrigins = DOMAINS.split(',');
+  if(allowedOrigins.indexOf(origin) > -1){
+     res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization,Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+      return res.sendStatus(204);
+  }
+  next();
+});
+*/
 function runServer( databaseUrl, port = PORT) {
-    /*
+    
     return new Promise((resolve, reject) => {
       mongoose.connect(databaseUrl,{ useNewUrlParser: true,useUnifiedTopology: true }, err => {
         if (err) {
@@ -34,13 +48,14 @@ function runServer( databaseUrl, port = PORT) {
           });
       });
     });
-    */
+    /*
     return new Promise((resolve,reject) => {
         server = app.listen(port, () => {
             console.log(`Your app is listening on port ${port}`);
             resolve();
         });
     });
+    */
   }
   
   function closeServer() {
@@ -58,7 +73,7 @@ function runServer( databaseUrl, port = PORT) {
   }
   
   if (require.main === module) {
-    runServer('DATABASE_URL').catch(err => console.error(err));
+    runServer(DATABASE_URL).catch(err => console.error(err));
   }
   
   module.exports = { app, runServer, closeServer };
